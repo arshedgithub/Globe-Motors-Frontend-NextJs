6c0f0cdcfb7c27c348b7b296d5815923d7834c06
arshedgithub <arshedahmedmail@gmail.com>
2024-08-28 16:45:19 +0530

    fetching products

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,36 @@
+'use client'
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export default function Login() {
-  return (
+
+    const router = useRouter();
+
+    function handleSignIn() {
+        // const user = await fetch('https')
+        router.push('/');
+    }
+
+    return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-md">
+      <Image className='m-auto' src="/globe-logo.png" alt='logo' width={100} height={100} />
+
         <h2 className="text-2xl font-bold text-center">Login</h2>
         <form className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm">
             <div>
-              <label htmlFor="email" className="sr-only">Email address</label>
+              <label htmlFor="username" className="sr-only">Username</label>
               <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
+                id="username"
+                name="username"
+                type="username"
+                autoComplete="username"
                 required
                 className="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-t-md appearance-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Email address"
+                placeholder="Username"
               />
             </div>
             <div className="-mt-px">
@@ -35,6 +49,7 @@ export default function Login() {
 
           <div>
             <button
+            onClick={handleSignIn}
               type="submit"
               className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import { getServerSideProps } from "next/dist/build/templates/pages";
 import AppBar from "./components/AppBar";
+
+async function getProducts() {
+  const res = await fetch("https://globe-motors-backend.vercel.app/api/products/", {
+    next: { revalidate: 30 }
+  });
+  return res.json();
+}
 
 export default function Home() {
 
-  
   return (
     <>
       <AppBar />
@@ -11,12 +16,14 @@ export default function Home() {
     </>);
 }
 
-const Products = () => {
+const Products = async () => {
+
+  const products = await getProducts();
+
   return (
     <div>
       <h1>Products</h1>
-      {/* Fetch data from an API */}
-      {/* Render products */}
+      {products.length}
     </div>
   )
 }
